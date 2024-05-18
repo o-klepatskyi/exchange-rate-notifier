4eb5d7c306aa13f1cc7b920fa6c37b73a34af203
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2024-05-18 22:30:35 +0300

    Cache API response and fetch current value every 5 minutes

---
+++ ratefetcher/ratefetcher.go
@@ -0,0 +1,57 @@
+package ratefetcher
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "time"
+)
+
+var cachedRate float64
+
+func fetchRate() {
+	fmt.Println("Fetching exchange rate")
+    client := &http.Client{
+        Timeout: 1 * time.Second,
+    }
+    resp, err := client.Get("https://api.monobank.ua/bank/currency")
+    if err != nil || resp.StatusCode != http.StatusOK {
+        fmt.Println("Error fetching data:", err, "status:", resp.StatusCode)
+        return
+    }
+    defer resp.Body.Close()
+
+    var rates []struct {
+        CurrencyCodeA int     `json:"currencyCodeA"`
+        CurrencyCodeB int     `json:"currencyCodeB"`
+        RateBuy       float64 `json:"rateBuy,omitempty"`
+    }
+
+    if err := json.NewDecoder(resp.Body).Decode(&rates); err != nil {
+        fmt.Println("Error parsing data:", err)
+        return
+    }
+
+    for _, rate := range rates {
+        if rate.CurrencyCodeA == 840 && rate.CurrencyCodeB == 980 {
+            cachedRate = rate.RateBuy
+            return
+        }
+    }
+
+    fmt.Println("Rate not found")
+}
+
+func StartRateFetcher() {
+    fetchRate()
+    // Monobank updates its cached rate every 5 minutes, no need to do it more often
+    ticker := time.NewTicker(5 * time.Minute)
+    for {
+        <-ticker.C
+        fetchRate()
+    }
+}
+
+func GetCachedRate() float64 {
+    return cachedRate
+}
